e1e68c7ea3601ac55b08691da9753c214110a9d1
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-11 12:38:52 -0500

    fixed merge conflicts

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http import HttpResponse
 # Create your views here.
+
+class Home(View):
+    def get(self, request):
+        return HttpResponse('homepage')
+   
+
