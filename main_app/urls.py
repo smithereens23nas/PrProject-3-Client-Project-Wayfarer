a01af9b8aedf11a674b23c1d37b384562b31a256
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-11 17:42:49 -0500

    login and autho set up

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home"),
-
+    path('/home/', views.Home.as_view(), name="home"),
+    # Home page when you login as a user.
+    # path('accounts/login/', views.Home.as_view(), name="Login"),
+    
 ]
