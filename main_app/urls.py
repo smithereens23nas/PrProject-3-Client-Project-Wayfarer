d4dc03c72b1b4378153ee10179b62585692c0b69
Smlcrp <smlcrp@gmail.com>
2022-02-14 10:50:17 -0500

    created countrylist, countrycreate, countrydetail templates and url paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/home/', views.Home.as_view(), name="home"),
+    path('home/', views.Home.as_view(), name="home"),
     # Home page when you login as a user.
     # path('accounts/login/', views.Home.as_view(), name="Login"),
+    path('countries/', views.CountryList.as_view(), name='country_list'),
+    path('countries/new', views.CountryCreate.as_view(), name='country_create'),
+    path('countries/<int:pk>/', views.CountryDetail.as_view(), name='country_detail'),
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
 from django.http import HttpResponse
+from django.urls import reverse
+from .models import Country, City
 # Create your views here.
 
 class Home(TemplateView):
@@ -24,4 +28,29 @@ countries = [
     Country( "Australia","https://cdn11.bigcommerce.com/s-plfev599h5/images/stencil/1280x1280/products/55544/168933/41MGRRitBpL__83715.1602574540.jpg?c=1",["Sydney", "Melburne", "Brisbane"],"Australia"),
 ]
 
+class CountryList(TemplateView):
+    template_name = 'country_list.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get('name')
+        if name != None:
+            context['countries'] = Country.objects.filter(name__icontains=name)
+            context['header'] = f"Searching For {name}"
+        else:
+            context['countries'] = Country.objects.all()
+            context['header'] = "All The Countries"
+        return context
+
+
+class CountryCreate(CreateView):
+    model = Country
+    fields = ['name', 'img', 'city', 'continent']
+    template_name = 'country_create.html'
+    def get_success_url(self):
+        return reverse('country_detail', kwargs={'pk': self.object.pk})
+
+
+class CountryDetail(DetailView):
+    model = Country
+    template_name = 'country_detail.html'
 
