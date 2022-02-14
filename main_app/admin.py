70f78d7529c8f6f5ea62cb66e66458534c0be16c
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-14 10:54:21 -0500

    Registered models for the admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, City, Country
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(City)
+admin.site.register(Country)
