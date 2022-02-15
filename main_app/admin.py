6200f2af6c55b8ff08afb0db3bbbe599f0e04b64
Jason <jay.garriques@gmail.com>
2022-02-15 16:10:45 -0500

    post models added in models and registered in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+@admin.register(models.Post)
+class Post(admin.ModelAdmin):
+    list_display = [ 'city', 'title', 'img','description']
+    city =models.CharField(max_length=30)
+    title =models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
 @admin.register(models.Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ['name', 'img', 'continent']
