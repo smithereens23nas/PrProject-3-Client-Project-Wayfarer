955e9a06bf89ba40af902f51004f39fa0687a667
Smlcrp <smlcrp@gmail.com>
2022-02-16 10:53:48 -0500

    got rid of useless stuff

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('edit/profile/', ProfileEdit.as_view(), name="profile_edit"),
     path('countries/', views.CountryList.as_view(), name='country_list'),
     path('profile/', views.Profile.as_view(), name='update_profile'),
-    path('countries/new', views.CountryCreate.as_view(), name='country_create'),
-    path('countries/<int:pk>/', views.CountryDetail.as_view(), name='country_detail'),
-    path('countries/<int:pk>/update', views.CountryUpdate.as_view(), name='country_update'),
-    path('countries/<int:pk>/delete', views.CountryDelete.as_view(), name='country_delete'),
+    # path('countries/new', views.CountryCreate.as_view(), name='country_create'),
+    # path('countries/<int:pk>/', views.CountryDetail.as_view(), name='country_detail'),
+    # path('countries/<int:pk>/update', views.CountryUpdate.as_view(), name='country_update'),
+    # path('countries/<int:pk>/delete', views.CountryDelete.as_view(), name='country_delete'),
     path('countries/<int:pk>/cities/new', views.CityCreate.as_view(), name='city_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -67,30 +67,30 @@ class CountryList(TemplateView):
         return context
 
 
-class CountryCreate(CreateView):
-    model = Country
-    fields = ['name', 'img', 'city', 'continent']
-    template_name = 'country_create.html'
-    def get_success_url(self):
-        return reverse('country_detail', kwargs={'pk': self.object.pk})
+# class CountryCreate(CreateView):
+#     model = Country
+#     fields = ['name', 'img', 'city', 'continent']
+#     template_name = 'country_create.html'
+#     def get_success_url(self):
+#         return reverse('country_detail', kwargs={'pk': self.object.pk})
 
 
-class CountryDetail(DetailView):
-    model = Country
-    template_name = 'country_detail.html'
+# class CountryDetail(DetailView):
+#     model = Country
+#     template_name = 'country_detail.html'
 
-class CountryUpdate(UpdateView):
-    model = Country
-    fields = ['name', 'img', 'city', 'continent']
-    template_name = 'country_update.html'
-    def get_success_url(self):
-        return reverse('country_detail', kwargs={'pk': self.object.pk})
+# class CountryUpdate(UpdateView):
+#     model = Country
+#     fields = ['name', 'img', 'city', 'continent']
+#     template_name = 'country_update.html'
+#     def get_success_url(self):
+#         return reverse('country_detail', kwargs={'pk': self.object.pk})
 
 
-class CountryDelete(DeleteView):
-    model = Country
-    template_name = 'country_delete_confirmation.html'
-    success_url = '/countries/'
+# class CountryDelete(DeleteView):
+#     model = Country
+#     template_name = 'country_delete_confirmation.html'
+#     success_url = '/countries/'
 
 
 class CityCreate(View):
